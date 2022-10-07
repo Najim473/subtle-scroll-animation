79848db7e7fd1a9fdf4ab7f99a22f1e5014545fe
najim473 <najim7304@gmail.com>
2022-10-07 19:35:49 +0600

    scroll animation done

---
+++ app.js
@@ -0,0 +1,33 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el))
+
+
+// SHOW BOTTOM 
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        }
+        else {
+            entry.target.classList.remove('show2');
+        }
+    })
+})
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+
+hiddenElements2.forEach((el) => observer2.observe(el))
